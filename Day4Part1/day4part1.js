15a25ca56dae00dec3ec8479d4ebbab7e4d6890a
CristianAUnisa <c.alfano@outlook.it>
2022-12-06 21:52:37 +0100

    Day 4 - First Part!

---
+++ Day4Part1/day4part1.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const readline_1 = require("readline");
+const rs = (0, fs_1.createReadStream)("input", { encoding: "utf-8" });
+const lines = (0, readline_1.createInterface)({ input: rs });
+let i = 0;
+lines.on("line", (line) => {
+    const [firstPair, secondPair] = line
+        .split(",")
+        .map((pair) => [...pair.split("-").map(num => +num)]);
+    if ((firstPair[0] >= secondPair[0] && firstPair[1] <= secondPair[1]) ||
+        (secondPair[0] >= firstPair[0] && secondPair[1] <= firstPair[1]))
+        i++;
+});
+lines.on("close", () => console.log(i));
---
+++ Day4Part1/day4part1.ts
@@ -0,0 +1,20 @@
+import { createReadStream } from "fs";
+import { createInterface } from "readline";
+
+const rs = createReadStream("input", { encoding: "utf-8" });
+const lines = createInterface({ input: rs });
+
+let i = 0;
+
+lines.on("line", (line) => {
+  const [firstPair, secondPair] = line
+    .split(",")
+    .map((pair) => [...pair.split("-").map(num => +num)]);
+  if (
+    (firstPair[0] >= secondPair[0] && firstPair[1] <= secondPair[1]) ||
+    (secondPair[0] >= firstPair[0] && secondPair[1] <= firstPair[1])
+  )
+    i++;
+});
+
+lines.on("close", () => console.log(i));
