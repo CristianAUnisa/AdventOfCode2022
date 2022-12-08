6948370d52945afaa601f6b25b436d2474169e04
CristianAUnisa <c.alfano@outlook.it>
2022-12-08 20:34:55 +0100

    Day 5 - Second Part!
    I just removed the reverse instruction from line 34

---
+++ Day5Part2/day5part2.ts
@@ -0,0 +1,37 @@
+import { createReadStream } from "fs";
+import { createInterface } from "readline";
+
+const rs = createReadStream("input", { encoding: "utf-8" });
+const lines = createInterface({ input: rs });
+
+const columns: Array<Array<string>> = [];
+const moves: Array<{start: number, end: number, quantity: number}> = [];
+
+lines.on("line", (line) => {
+  if (line.length == 1) {
+    return
+  } else if (line.startsWith("move")) {
+    const [quantity, start, end] = line.match(/\d+/g)!.map((char) => +char);
+    moves.push({quantity, start, end});
+  } else {
+    if (columns.length == 0) {
+      columns.push(
+        ...Array.from({ length: (line.length / 4) + 1 }).map(() => [])
+      );
+    }
+    for (let i = 1; i < line.length; i += 4) {
+      const char = line.charAt(i);
+      if ("[] ".indexOf(char) == -1 && isNaN(+char)) {
+        columns[(i - 1) / 4].push(char);
+      }
+    }
+  }
+});
+
+let a = 0;
+lines.on("close", () => {
+  moves.forEach((move) => {
+    columns[move.end - 1].unshift(...columns[move.start - 1].splice(0, move.quantity));
+  });
+  console.log(columns.map((column) => column[0]).join(""));
+});
