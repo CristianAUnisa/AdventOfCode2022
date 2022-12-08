7e7cf302a6199f7c0a148b2e1cd76575e318edf1
CristianAUnisa <c.alfano@outlook.it>
2022-12-08 20:27:47 +0100

    Day 5 - First Part!

--- Day5Part1/day5part1.ts
+++ Day5Part1/day5part1.ts
@@ -4,16 +4,34 @@ import { createInterface } from "readline";
 const rs = createReadStream("input", { encoding: "utf-8" });
 const lines = createInterface({ input: rs });
 
-let i = 0;
+const columns: Array<Array<string>> = [];
+const moves: Array<{start: number, end: number, quantity: number}> = [];
 
 lines.on("line", (line) => {
-  const [firstPair, secondPair] = line
-    .split(",")
-    .map((pair) => [...pair.split("-").map(num => +num)]);
-  if (
-    (firstPair[0] <= secondPair[1] && firstPair[1] >= secondPair[0])
-  )
-    i++;
+  if (line.length == 1) {
+    return
+  } else if (line.startsWith("move")) {
+    const [quantity, start, end] = line.match(/\d+/g)!.map((char) => +char);
+    moves.push({quantity, start, end});
+  } else {
+    if (columns.length == 0) {
+      columns.push(
+        ...Array.from({ length: (line.length / 4) + 1 }).map(() => [])
+      );
+    }
+    for (let i = 1; i < line.length; i += 4) {
+      const char = line.charAt(i);
+      if ("[] ".indexOf(char) == -1 && isNaN(+char)) {
+        columns[(i - 1) / 4].push(char);
+      }
+    }
+  }
 });
 
-lines.on("close", () => console.log(i));
+let a = 0;
+lines.on("close", () => {
+  moves.forEach((move) => {
+    columns[move.end - 1].unshift(...columns[move.start - 1].splice(0, move.quantity).reverse());
+  });
+  console.log(columns.map((column) => column[0]).join(""));
+});
